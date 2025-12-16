52c10ae6c83a30a2b1c161b5b2687551870f66b3
Emmanuel Mhango <157731314+emmanuel-mhango@users.noreply.github.com>
2025-12-16 12:11:51 +0200

    Removing some of the universities to those that only are well known

--- src/data/universities.ts
+++ src/data/universities.ts
@@ -5,21 +5,11 @@ export const malawianUniversities = [
   { value: "MZUNI", label: "Mzuzu University (MZUNI)", type: "Public" },
   { value: "MUST", label: "Malawi University of Science and Technology (MUST)", type: "Public" },
   { value: "LUANAR", label: "Lilongwe University of Agriculture and Natural Resources (LUANAR)", type: "Public" },
-  { value: "MWU", label: "Malawi University (MWU)", type: "Public" },
-  
+
   // Private Universities
   { value: "DMI", label: "DMI-St. John the Baptist University", type: "Private" },
   { value: "AFRICAN_BIBLE", label: "African Bible College", type: "Private" },
   { value: "CATHOLIC_UNI", label: "Catholic University of Malawi", type: "Private" },
-  { value: "UNICAF", label: "Unicaf University", type: "Private" },
-  { value: "EXPLOITS", label: "Exploits University", type: "Private" },
-  { value: "LIVINGSTONIA", label: "Livingstonia University", type: "Private" },
-  { value: "SHAREWORLD", label: "Shareworld Open University", type: "Private" },
   { value: "MALAWI_ADVENTIST", label: "Malawi Adventist University", type: "Private" },
-  { value: "BLANTYRE_INTERNAT", label: "Blantyre International University", type: "Private" },
-  { value: "MCHS_UNI", label: "MCHS University", type: "Private" },
-  { value: "PENTECOSTAL", label: "Pentecostal Life University", type: "Private" },
-  { value: "SKYWAY", label: "Skyway University", type: "Private" },
-  { value: "WISDOM", label: "Wisdom University", type: "Private" },
-  { value: "DAEYANG", label: "Daeyang University", type: "Private" }
-];
+  
+]
